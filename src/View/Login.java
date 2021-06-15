821ee52ca282246eda6fc9746c4566ea3bc35712
Farras Danyka Putra <71580610+farrasdny@users.noreply.github.com>
2021-06-16 00:38:03 +0800

    Update Login.java

--- src/View/Login.java
+++ src/View/Login.java
@@ -1,5 +1,6 @@
 package View;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,14 +11,32 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class Login{
+public class LoginMahasiswa extends Application{
+    // variabel pane login mahasiswa
+    private AnchorPane anchor;
     
-    
-    
-    public void component(){
+    public void component() throws IOException {
+        // =============================================================================
+        //                           DEKLARASI VARIABEL
+        // =============================================================================
         
-        
-    
-        
+         anchor = new AnchorPane();
+         
+        // =============================================================================
+        //                          CONTROL PANE LOGIN
+        // =============================================================================
+         anchor.setPrefSize(1100 , 800);
+         anchor.setStyle("-fx-background-color: linear-gradient(#C54A4A, #431616);");
     }     
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        component();
+        
+        Scene scene = new Scene(anchor);
+        
+        primaryStage.setTitle("Program Pengajuan PKN");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
