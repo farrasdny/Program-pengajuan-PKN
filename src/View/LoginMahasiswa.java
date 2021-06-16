7f762f83203531c0646d64181113742067bb7cbe
Farras Danyka Putra <danyka486@gmail.com>
2021-06-16 20:08:27 +0800

    Tugas akhir PBO

---
+++ src/View/LoginMahasiswa.java
@@ -0,0 +1,152 @@
+package View;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+public abstract class LoginMahasiswa extends Application{
+    
+    // variabel pane login mahasiswa
+    private AnchorPane anchor;
+    private Pane bannerAtas;
+    private Pane bannerBawah;
+    private Label judulDepan;
+    private Label judulDepan2;
+    private TextField namaField;
+    private TextField nimField;
+    private TextField jurusanField;
+    private Label jurusanLabel;
+    private Button simpanButton;
+    private Button batalButton;
+    
+    public void component(){
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        
+        // =============================================================================
+        //                           DEKLARASI VARIABEL
+        // =============================================================================
+        
+        anchor = new AnchorPane();
+        bannerAtas = new Pane();
+        bannerBawah = new Pane();
+        judulDepan = new Label("INPUT DATA");
+        judulDepan2 = new Label("MAHASISWA");
+        namaField = new TextField();
+        nimField = new TextField();
+        jurusanField = new TextField();
+        jurusanLabel = new Label("jurusan");
+        simpanButton = new Button("SIMPAN");
+        batalButton = new Button("BATAL");
+        
+        // =============================================================================
+        //                          CONTROL PANE LOGIN
+        // =============================================================================
+        
+        anchor.setPrefSize(1100 , 800);
+        anchor.setStyle("-fx-background-color: linear-gradient(#4C87EB, #242275);");
+        anchor.getChildren().addAll(
+                bannerAtas, bannerBawah, judulDepan, judulDepan2, namaField, nimField, jurusanField,
+                simpanButton, batalButton
+        );
+        
+        bannerAtas.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY,Insets.EMPTY)));
+        bannerAtas.setPrefSize(1100, 50);
+        
+        bannerBawah.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY,Insets.EMPTY)));
+        bannerBawah.setPrefSize(1100, 50);
+        bannerBawah.setLayoutY(750);
+        
+        judulDepan.setFont(Font.font("Poppins", FontWeight.LIGHT, 40));
+        judulDepan.setTextFill(Color.WHITE);
+        judulDepan.setLayoutX(305);
+        judulDepan.setLayoutY(410);
+         
+        judulDepan2.setFont(Font.font("Poppins", FontWeight.BOLD, 40));
+        judulDepan2.setTextFill(Color.WHITE);
+        judulDepan2.setLayoutX(540);
+        judulDepan2.setLayoutY(410);
+        
+        ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/View/logo2.png")));
+        anchor.getChildren().add(image);
+        image.setFitHeight(280);
+        image.setFitWidth(290);
+        image.setLayoutX(400);
+        image.setLayoutY(100);
+        
+        namaField.setPrefSize(260, 30);
+        namaField.setPromptText("nama");
+        namaField.setFont(Font.font("Poppins", FontWeight.SEMI_BOLD, 14));
+        namaField.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(5),Insets.EMPTY)));
+        namaField.setLayoutY(490);
+        namaField.setLayoutX(420);
+        
+        nimField.setPrefSize(260, 30);
+        nimField.setPromptText("NIM");
+        nimField.setFont(Font.font("Poppins", FontWeight.SEMI_BOLD, 14));
+        nimField.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(5),Insets.EMPTY)));
+        nimField.setLayoutY(540);
+        nimField.setLayoutX(420);
+        
+        jurusanField.setPrefSize(260, 30);
+        jurusanField.setPromptText("jurusan");
+        jurusanField.setFont(Font.font("Poppins", FontWeight.SEMI_BOLD, 14));
+        jurusanField.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(5),Insets.EMPTY)));
+        jurusanField.setLayoutY(590);
+        jurusanField.setLayoutX(420);
+        
+        jurusanLabel.setFont(Font.font("Poppins", FontWeight.LIGHT, 20));
+        jurusanLabel.setTextFill(Color.WHITE);
+        jurusanLabel.setLayoutX(380);
+        jurusanLabel.setLayoutY(555);
+        
+        simpanButton.setPrefSize(125, 30);
+        simpanButton.setBackground(new Background(new BackgroundFill(Color.CORNFLOWERBLUE, new CornerRadii(5),Insets.EMPTY)));
+        simpanButton.setFont(Font.font("Poppins", FontWeight.LIGHT, 13));
+        simpanButton.setTextFill(Color.WHITE);
+        simpanButton.setLayoutX(554);
+        simpanButton.setLayoutY(638);
+        
+        batalButton.setPrefSize(125, 30);
+        batalButton.setBackground(new Background(new BackgroundFill(Color.ORANGE, new CornerRadii(5),Insets.EMPTY)));
+        batalButton.setFont(Font.font("Poppins", FontWeight.LIGHT, 13));
+        batalButton.setTextFill(Color.WHITE);
+        batalButton.setLayoutX(420);
+        batalButton.setLayoutY(638);
+        
+        // =============================================================================
+        //                                  OPERATION
+        // =============================================================================
+        
+        batalButton.setOnMousePressed((MouseEvent event) -> {
+            window.close();
+            MainClass main = new MainClass();
+            main.accFromOtherClass();
+        });
+        
+        Scene scene = new Scene(anchor);
+        window.setScene(scene);
+        window.show();
+    }     
+}
---
+++ src/View/MainClass.java
@@ -0,0 +1,175 @@
+package View;
+
+import View.LoginMahasiswa;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MSI
+ */
+
+public class MainClass extends Application {
+    
+    // variabel pane utama
+    private AnchorPane anchor;
+    private Label judulDepan;
+    private Label judulDepan2;
+    private Label info;
+    private ComboBox<String> cbox;
+    private Pane bannerAtas;
+    private Pane bannerBawah;
+    private Button buttonPilih;
+
+    public void component(){
+        
+        // =============================================================================
+        //                           DEKLARASI VARIABEL
+        // =============================================================================
+        
+         anchor = new AnchorPane();
+         judulDepan = new Label("PENGAJUAN PELAKSANAAN PKN");
+         judulDepan2 = new Label("( Praktek Kerja Nyata )");
+         info = new Label("Login sebagai");
+         cbox = new ComboBox<>();
+         info = new Label("Login sebagai :");
+         bannerAtas = new Pane();
+         bannerBawah = new Pane();
+         buttonPilih = new Button("PILIH");
+     
+        // =============================================================================
+        //                          CONTROL PANE UTAMA
+        // =============================================================================
+           
+         anchor.setPrefSize(1100 , 800);
+         anchor.setStyle("-fx-background-color: linear-gradient(#4C87EB, #242275);");
+         anchor.getChildren().addAll(judulDepan, judulDepan2, cbox, info, bannerAtas, bannerBawah, buttonPilih);
+         
+         judulDepan.setFont(Font.font("Poppins", FontWeight.BOLD, 40));
+         judulDepan.setTextFill(Color.WHITE);
+         judulDepan.setLayoutX(230);
+         judulDepan.setLayoutY(390);
+         
+         judulDepan2.setFont(Font.font("Poppins", FontWeight.LIGHT, 30));
+         judulDepan2.setTextFill(Color.WHITE);
+         judulDepan2.setLayoutX(400);
+         judulDepan2.setLayoutY(440);
+         
+         cbox.setPromptText("Select");
+         cbox.getItems().addAll("Mahasiswa", "Dosen", "Admin");
+         cbox.setPrefSize(220, 30);
+         cbox.setLayoutX(437);
+         cbox.setLayoutY(600);
+         cbox.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, new CornerRadii(5),Insets.EMPTY)));
+         
+         // --> Azzahry Dwi Pramudio
+                
+         // slot image
+         ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/View/logo2.png")));
+         anchor.getChildren().add(image);
+         image.setFitHeight(280);
+         image.setFitWidth(290);
+         image.setLayoutX(400);
+         image.setLayoutY(100);
+         
+         // slot info
+         info.setFont(Font.font("Poppins", FontWeight.EXTRA_LIGHT, 20));
+         info.setTextFill(Color.WHITE);
+         info.setLayoutX(480);
+         info.setLayoutY(560);
+         
+         // slot banner atas
+         bannerAtas.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY,Insets.EMPTY)));
+         bannerAtas.setPrefSize(1100, 50);
+         
+         // slot banner bawah
+         bannerBawah.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY,Insets.EMPTY)));
+         bannerBawah.setPrefSize(1100, 50);
+         bannerBawah.setLayoutY(750);
+         
+         // slot button
+         buttonPilih.setPrefSize(220, 30);
+         buttonPilih.setLayoutX(437);
+         buttonPilih.setLayoutY(643);
+         buttonPilih.setBackground(new Background(new BackgroundFill(Color.CORNFLOWERBLUE, new CornerRadii(5),Insets.EMPTY)));
+         buttonPilih.setFont(Font.font("Poppins", FontWeight.LIGHT, 13));
+         buttonPilih.setTextFill(Color.WHITE);
+    }
+    
+    public void slideLoginMahasiswa(){
+        LoginMahasiswa login = new LoginMahasiswa() {
+            @Override
+            public void start(Stage primaryStage) throws Exception {
+                //...
+            }
+        };
+        login.component();
+    }
+    
+    public void accFromOtherClass(){
+        component();
+        Stage window = new Stage();
+        Scene scene = new Scene(anchor);
+        
+        buttonPilih.setOnMousePressed((MouseEvent event) -> { 
+            window.close();
+            slideLoginMahasiswa();
+        });
+                
+        window.setTitle("Program Pengajuan PKN");
+        window.setScene(scene);
+        window.show();
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        component();
+        Scene scene = new Scene(anchor);
+        
+        buttonPilih.setOnMousePressed((MouseEvent event) -> { 
+            primaryStage.close();
+            
+            // combo box
+            switch(cbox.getValue()){
+                case "Mahasiswa":
+                    slideLoginMahasiswa();
+                default:
+                    break;
+            }   
+        });
+        
+        primaryStage.setTitle("Program Pengajuan PKN");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+        
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        launch(args);
+    }    
+}
